0ee0c4322607fe151fce3b05f9b377174ee3efb2
live4qy <1404857825@qq.com>
2021-10-03 14:58:10 +0800

    upd

---
+++ Codeforces/1579/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define PI pair<int,int>
+// #define int long long
+using namespace std;
+
+
+void solve(){
+    int x;cin>>x;
+    cout<<x<<endl;
+}
+void Main(){
+    solve();
+}
+void Init(){
+    freopen("../in.txt","r",stdin);
+    freopen("../out.txt","w",stdout);
+}
+signed main(){
+    Init();
+    Main();
+    return 0;
+}
